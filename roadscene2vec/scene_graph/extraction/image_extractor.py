8c6e21d17d18b4dafc87bc51149d3468174fd997
Trey Woodlief <adwiii7@gmail.com>
2023-02-24 13:42:05 -0500

    Edits to use panoptic

--- roadscene2vec/scene_graph/extraction/image_extractor.py
+++ roadscene2vec/scene_graph/extraction/image_extractor.py
@@ -1,5 +1,4 @@
 import os
-import pdb
 import sys
 from pathlib import Path
 import pickle
@@ -31,14 +30,17 @@ class RealExtractor(ex):
         # if not os.path.exists(self.input_path):
         #     raise FileNotFoundError(self.input_path)
 
-        # detectron setup
-        model_path = 'COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml'
+        # [CM]: Select your Detectron2 segmentation model
+        model_path = 'COCO-PanopticSegmentation/panoptic_fpn_R_50_1x.yaml'
         self.cfg = get_cfg()
         self.cfg.merge_from_file(model_zoo.get_config_file(model_path))
         self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
         self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(model_path)
-        self.coco_class_names = MetadataCatalog.get(self.cfg.DATASETS.TRAIN[0]).get('thing_classes')
         self.predictor = DefaultPredictor(self.cfg)
+
+        # [CM]: Select all objects and classes for segementation; desired classes are ultimately selected in "use_case_1_scenegraph_extraction_config.yaml"
+        self.coco_class_names = MetadataCatalog.get(self.cfg.DATASETS.TRAIN[0]).get('thing_classes')
+        self.coco_class_names += MetadataCatalog.get(self.cfg.DATASETS.TRAIN[0]).get('stuff_classes')
 
         # bev setup
         if dataset_type:
@@ -57,21 +59,16 @@ class RealExtractor(ex):
                 seq = int(path.stem.split('_')[0])
                 label_path = (path/"label.txt").resolve()
                 ignore_path = (path/"ignore.txt").resolve()
-                if ignore_path.exists(): #record ignored sequences, and only load the sequences that were not ignored
-                    with open(str(path/"ignore.txt"), 'r') as label_f:
-                        ignore_label = int(label_f.read())
-                        if ignore_label:
-                            self.dataset.ignore.append(seq)
-                            continue #skip to next seq if ignore path exists
+
                 seq_images = self.load_images(path)
-            
+
                 self.dataset.scene_graphs[seq] = {}
                 for frame, img in seq_images.items():
                     out_img_path = None
-                    bounding_boxes = self.get_bounding_boxes(img_tensor=img, out_img_path=out_img_path)
-                    
+                    bounding_boxes, segmentation = self.get_bounding_boxes(img_tensor=img, out_img_path=out_img_path)
                     scenegraph = SceneGraph(self.relation_extractor,    
                                                 bounding_boxes = bounding_boxes, 
+                                                segmentation = segmentation,
                                                 bev = self.bev,
                                                 coco_class_names=self.coco_class_names, 
                                                 platform=self.dataset_type)
@@ -85,17 +82,17 @@ class RealExtractor(ex):
                         self.dataset.labels[seq] = l0
 
         except Exception as e:
-            pdb.set_trace()
             import traceback
             print('We have problem creating the real image scenegraphs')
             print(e)
             traceback.print_exc()
-    
+
     #returns a numpy array representation of a sequence of images in format (H,W,C)
     def load_images(self, path):
         raw_images_loc = (path/'raw_images').resolve()
-        images = sorted([Path(f) for f in os.listdir(raw_images_loc) if isfile(join(raw_images_loc, f)) and ".DS_Store" not in f and "Thumbs" not in f], key = lambda x: int(x.stem.split(".")[0]))
-        images = [join(raw_images_loc,i) for i in images] 
+        images = sorted([Path(f) for f in os.listdir(raw_images_loc) if isfile(join(raw_images_loc, f))], key = lambda x: int(x.stem.split(".")[0]))
+        images = [join(raw_images_loc,i) for i in images]
+
         sequence_tensor = {}
         modulo = 0
         acc_number = 0
@@ -103,6 +100,7 @@ class RealExtractor(ex):
             modulo = int(len(images) / self.framenum)  #subsample to frame limit
         if(self.framenum == None or modulo == 0):
             modulo = 1
+
         for i in range(0, len(images)):
             if (i % modulo == 0 and self.framenum == None) or (i % modulo == 0 and acc_number < self.framenum):
                 image_path = images[i]
@@ -111,6 +109,14 @@ class RealExtractor(ex):
                 sequence_tensor[frame_num] = im 
                 acc_number += 1
         return sequence_tensor
+            
+
+    def get_seg_annotated_image(self, im):
+        v = visualizer.Visualizer(im[:, :, ::-1], 
+            MetadataCatalog.get(self.cfg.DATASETS.TRAIN[0]), 
+            scale=1)
+        out = v.draw_panoptic_seg(self.predictor(im)['panoptic_seg'][0].to('cpu'), self.predictor(im)['panoptic_seg'][1])
+        return out.get_image()[:, :, ::-1]
 
     def get_bounding_box_annotated_image(self, im):
         v = visualizer.Visualizer(im[:, :, ::-1], 
@@ -122,19 +128,19 @@ class RealExtractor(ex):
     def get_bounding_boxes(self, img_tensor, out_img_path=None):
         im = img_tensor
         outputs = self.predictor(im)
+
         if out_img_path:
             # We can use `Visualizer` to draw the predictions on the image.
             out = self.get_bounding_box_annotated_image(im)
             cv2.imwrite(out_img_path, out)
 
-        # todo: after done scp to server
-        # crop im to remove ego car's hood
-        # find threshold then remove from pred_boxes, pred_classes, check image_size
-        bounding_boxes = outputs['instances'].pred_boxes, outputs['instances'].pred_classes, outputs['instances'].image_size
-        return bounding_boxes
+        segmentation = outputs['panoptic_seg']
+        bounding_boxes = outputs['instances'].pred_boxes, outputs['instances'].pred_classes, outputs['instances'].image_size, outputs['instances'].pred_masks
+
+        return bounding_boxes, segmentation
 
     
-    '''Returns SceneGraphDataset object containing scengraphs, labels, and action types'''
+    '''Returns SceneGraphDataset object containing scenegraphs, labels, and action types'''
     def getDataSet(self):
         try:
             return self.dataset
